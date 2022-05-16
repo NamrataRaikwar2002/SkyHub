76678641c53a14c71460e21d81bbe2fce95d8595
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-05-16 19:05:05 +0530

    feat: export all pages in one

---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export { Login } from './Auth/Login'
+export { Signup } from './Auth/Signup'
+export { Home } from './Home'
+export { Profile } from './Profile'
+export { BookMark } from './BookMark'
+export { Explore } from './Explore'
