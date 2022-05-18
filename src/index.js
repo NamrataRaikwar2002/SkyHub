42e14e945a2819f217ac467dbc1ea01ce826bb11
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-05-18 17:14:45 +0530

    feat:provide redux store to react

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { makeServer } from './server'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/app/store'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
@@ -13,10 +15,12 @@ makeServer()
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
